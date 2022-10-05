47c51f153af5ceb4c8ed76798bfd108e8d97f3c8
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-10-05 18:03:21 +0300

    Updated Board controller to use request and response objects.

--- src/main/java/ro/dev/trellteam/web/controller/BoardController.java
+++ src/main/java/ro/dev/trellteam/web/controller/BoardController.java
@@ -11,69 +11,69 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import ro.dev.trellteam.helper.GeneralHelper;
 import ro.dev.trellteam.domain.Board;
+import ro.dev.trellteam.web.dto.BoardDto;
+import ro.dev.trellteam.web.mapper.BoardMapper;
+import ro.dev.trellteam.web.response.board.CreateBoardResponse;
+import ro.dev.trellteam.web.response.board.ListBoardsResponse;
 import ro.dev.trellteam.web.service.BoardService;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/board", produces = APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/board/v1", produces = APPLICATION_JSON_VALUE)
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Slf4j
 public class BoardController {
     private final BoardService boardService;
+    private final BoardMapper boardMapper;
 
-    @PostMapping("/main")
-    public ResponseEntity<Board> createBoard(@RequestBody Map<String, String> payload) {
+    @PostMapping()
+    public ResponseEntity<CreateBoardResponse> createBoard(@RequestBody @Valid final BoardDto requestBody) {
         log.debug("BoardController--createBoard--IN");
 
-        final Long idDep = Long.parseLong(payload.get("idDep"));
-        final String title = payload.get("title");
-        final String version = payload.get("version");
-        final String release = payload.get("release");
-
-        final URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/board/main").toUriString());
+        final URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/board/v1").toUriString());
         log.debug("BoardController--createBoard--uri: {}", uri);
 
-        log.debug("BoardController--createBoard--idDep: {}", idDep);
-        log.debug("BoardController--createBoard--title: {}", title);
-        log.debug("BoardController--createBoard--version: {}", version);
-        log.debug("BoardController--createBoard--release: {}", release);
+        log.debug("BoardController--createBoard--idDep: {}", requestBody.getIdDep());
+        log.debug("BoardController--createBoard--title: {}", requestBody.getTitle());
+        log.debug("BoardController--createBoard--version: {}", requestBody.getVersion());
+        log.debug("BoardController--createBoard--release: {}", requestBody.getRelease());
 
-        Board board = new Board();
+        Board board = boardMapper.dtoToDomain(requestBody);
         board.setDateCreated(new java.sql.Date((new Date()).getTime()));
-        board.setTitle(title);
-        board.setIdDep(idDep);
-        board.setVersion(version);
-        board.setRelease(new java.sql.Date(GeneralHelper.convertStringToDate(release, "yyyy-MM-dd").getTime()));
-
-        log.debug("BoardController--createBoard--board: {}", board);
 
         board = boardService.createBoard(board);
-
         log.debug("BoardController--createBoard--board: {}", board.toString());
-        log.debug("BoardController--createBoard--OUT");
 
-        return ResponseEntity.created(uri).body(board);
+        final CreateBoardResponse response = new CreateBoardResponse(boardMapper.domainToDto(board));
+
+        log.debug("BoardController--createBoard--OUT");
+        return ResponseEntity.created(uri).body(response);
     }
 
     @GetMapping("/department/{idDepartment}")
-    public ResponseEntity<List<Board>> listBoardsByDepartment(@PathVariable Long idDepartment) {
+    public ResponseEntity<ListBoardsResponse> listBoardsByDepartment(@PathVariable Long idDepartment) {
         log.debug("BoardController--listBoardsByDepartment--IN");
         log.debug("BoardController--listBoardsByDepartment--idDepartment: {}", idDepartment);
 
         final List<Board> boards = boardService.listDepartmentBoards(idDepartment);
-
         log.debug("BoardController--listBoardsByDepartment--boards: {}", boards.toString());
-        log.debug("BoardController--listBoardsByDepartment--OUT");
 
-        return ResponseEntity.ok().body(boards);
+        final ListBoardsResponse response = new ListBoardsResponse(
+                boards.stream()
+                        .map(boardMapper::domainToDto)
+                        .collect(Collectors.toList())
+        );
+
+        log.debug("BoardController--listBoardsByDepartment--OUT");
+        return ResponseEntity.ok().body(response);
     }
 }
