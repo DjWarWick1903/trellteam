ca72b8e3dfe7cce60dd94423e0d9d83556b8bcd1
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-10-22 01:15:21 +0300

    Created error helper class which contains error codes and description.

---
+++ src/main/java/ro/dev/trellteam/exceptions/ErrorHelper.java
@@ -0,0 +1,20 @@
+package ro.dev.trellteam.exceptions;
+
+import java.util.HashMap;
+
+/**
+ * Class which contains every error code and it's description.
+ */
+public class ErrorHelper {
+
+    private static HashMap<String, String> errorList;
+
+    static {
+        errorList = new HashMap<>();
+
+        errorList.put("TRELL_ERR_0", "An error has been encountered.");
+        errorList.put("TRELL_ERR_1", "Card type was not found.");
+        errorList.put("TRELL_ERR_2", "Card was not found.");
+        errorList.put("TRELL_ERR_3", "Account was not found.");
+    }
+}
